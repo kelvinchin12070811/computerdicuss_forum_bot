b31f0f058d9e9550cc597cdbbe8ef36f8ec27686
Kelvin Chin <chinkaiwen@yahoo.com>
2021-09-22 21:56:43 +0800

    🎉 Completed add sticker command

--- src/commands/add_sticker.ts
+++ src/commands/add_sticker.ts
@@ -4,12 +4,14 @@ import {
     Permissions,
 } from 'discord.js';
 
-export const addSticker = async (interaction: CommandInteraction, client: Client) => {
-    const author = interaction.user;
-    const server = interaction.guild;
+import Sticker from '../db/datatype/sticker';
 
+export const addSticker = async (interaction: CommandInteraction, client: Client) => {
     if (!(interaction.member?.permissions as Permissions).has(Permissions.STAGE_MODERATOR))
         return;
 
-    interaction.reply('hi admin!');
+    const stickerName = interaction.options.get('sticker_name')?.value as string;
+    const stickerURI = interaction.options.get('uri')?.value as string;
+    await Sticker.create({ keyword: stickerName, uri: stickerURI });
+    await interaction.reply('Sticker added to library');
 }
