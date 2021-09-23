71e212e13406e393474720e4f665464b688bceb7
Kelvin Chin <chinkaiwen@yahoo.com>
2021-09-24 01:33:23 +0800

    🚚 Migrate sticker command to CommandFactory pattern

--- src/commands/sticker.ts
+++ src/commands/sticker.ts
@@ -1,7 +1,18 @@
+import {
+    Client,
+    CommandInteraction,
+} from 'discord.js';
+
 import Sticker from '../db/datatype/sticker';
 
-export const returnSticker = async (stickerName: string): Promise<string> => {
-    const sticker = await Sticker.findOne({ where: { keyword: stickerName.toLowerCase() } });
-    if (sticker == null) return `Sticker "${stickerName}" does not exist.`;
-    return (sticker as any).uri as string;
+export const returnSticker = async (interaction: CommandInteraction, client: Client) => {
+    const stickerName = (interaction.options.get('sticker_name')?.value as string).toLowerCase();
+    let message = '';
+
+    const sticker = await Sticker.findOne({ where: { keyword: stickerName } });
+
+    if (sticker == null) message = `Sticker "${stickerName}" does not exist.`;
+    else message = (sticker as any).uri as string;
+
+    await interaction.reply(message);
 }
