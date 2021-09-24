e5e66bfc22c4c1983e3249d877c1e129913c66ec
Kelvin Chin <chinkaiwen@yahoo.com>
2021-09-24 22:14:18 +0800

    📜 Added license header to all source files.

--- src/commands.ts
+++ src/commands.ts
@@ -1,3 +1,8 @@
+/***********************************************************************************************************************
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ **********************************************************************************************************************/
 // Force registration in the commands to run.
 import './commands/add_sticker';
 import './commands/sticker';
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+/***********************************************************************************************************************
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ **********************************************************************************************************************/
 import { REST } from '@discordjs/rest';
 import { Routes } from 'discord-api-types/v9';
 import { config } from 'dotenv';
