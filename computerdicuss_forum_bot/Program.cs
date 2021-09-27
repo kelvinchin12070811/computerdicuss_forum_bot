8a56509389a1d99bda84b35ebc98d7d240094803
Kelvin Chin <chinkaiwen@yahoo.com>
2021-09-27 20:17:17 +0800

    Change base code to use DependencyInjection

--- computerdicuss_forum_bot/Program.cs
+++ computerdicuss_forum_bot/Program.cs
@@ -1,89 +1,10 @@
 ﻿using System;
 using System.Threading.Tasks;
-using Discord;
-using Discord.WebSocket;
-using Discord.Commands;
-using System.Threading;
 
 namespace ComputerDiscussForumBot
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Log.Instance.Init();
-            Config.Instance.Init();
-
-            Log.Instance.Logger.Info("Starting bot...");
-            new Program().BotMain().GetAwaiter().GetResult();
-        }
-
-        public async Task BotMain()
-        {
-            var cancelToken = new CancellationTokenSource();
-            Console.CancelKeyPress += delegate {
-                cancelToken.Cancel();
-                Log.Instance.Logger.Info("Logging out from Discord...");
-                Client.Instance.BotClient.LogoutAsync();
-                Log.Instance.Logger.Info("Logged out from discord.");
-            };
-
-            try
-            {
-                Log.Instance.Logger.Info("Initializing bot...");
-                Client.Instance.Init();
-                Client.Instance.BotClient.Ready += OnReady;
-                Client.Instance.BotClient.LoggedOut += OnLoggedOut;
-                Client.Instance.BotClient.MessageReceived += OnMessageReceived;
-
-                Log.Instance.Logger.Info("Logging into Discord...");
-                await Client.Instance.BotClient.LoginAsync(TokenType.Bot, Config.Instance.Token);
-                await Client.Instance.BotClient.StartAsync();
-            }
-            catch (Exception e)
-            {
-                Log.Instance.Logger.Error("Critical error occured.", e);
-            }
-
-            try
-            {
-                await Task.Delay(-1, cancelToken.Token);
-            }
-            catch (TaskCanceledException e)
-            {
-                Log.Instance.Logger.Info("Interupt request accepted.", e);
-                return;
-            }
-            catch (Exception e)
-            {
-                Log.Instance.Logger.Fatal("Critical error occured.", e);
-                Environment.Exit(1);
-            }
-        }
-
-        private async Task OnMessageReceived(SocketMessage msg)
-        {
-            var usrmsg = msg as SocketUserMessage;
-            if (usrmsg.Author.IsBot) return;
-
-            var context = new SocketCommandContext(Client.Instance.BotClient, usrmsg);
-            int pos = 0;
-
-            if (context.Message.Content == "hi")
-                await context.Message.ReplyAsync("Hi there!");
-        }
-
-        private Task OnLoggedOut()
-        {
-            Log.Instance.Logger.Info("Logged out from Discord!");
-            return Task.CompletedTask;
-        }
-
-        private Task OnReady()
-        {
-            var currentUser = Client.Instance.BotClient.CurrentUser;
-            Log.Instance.Logger.Info($"Logged in as { currentUser.Username }#{currentUser.Discriminator}!");
-            return Task.CompletedTask;
-        }
+        public static async Task Main(string[] args) => await Startup.RunAsync(args);
     }
 }
---
+++ computerdicuss_forum_bot/Services/StartupService.cs
@@ -0,0 +1,37 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerDiscussForumBot.Services
+{
+    public class StartupService
+    {
+        public static IServiceProvider provider;
+        private readonly DiscordSocketClient discord;
+        private readonly CommandService commands;
+        private readonly IConfigurationRoot config;
+
+        public StartupService(IServiceProvider provider, DiscordSocketClient discord, CommandService commands,
+            IConfigurationRoot config)
+        {
+            StartupService.provider = provider;
+            this.discord = discord;
+            this.commands = commands;
+            this.config = config;
+        }
+
+        public async Task StartAsync()
+        {
+            string token = config["token"];
+            if (String.IsNullOrEmpty(token))
+            {
+                Console.WriteLine("Token is empty");
+            }
+        }
+    }
+}
