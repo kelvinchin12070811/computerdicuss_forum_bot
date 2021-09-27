83a27f0a25b8d76c37ffa3ac1f19652b2f79e8c7
Kelvin Chin <chinkaiwen@yahoo.com>
2021-09-28 01:48:21 +0800

    Added license header

--- computerdicuss_forum_bot/Commands/General.cs
+++ computerdicuss_forum_bot/Commands/General.cs
@@ -1,4 +1,9 @@
-﻿using Discord;
+﻿/***********************************************************************************************************************
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ **********************************************************************************************************************/
+using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
 using log4net;
