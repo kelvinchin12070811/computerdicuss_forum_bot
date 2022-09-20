db8bb930a8a3e83ee1e9fe525cfacdda12cd5ab6
Kelvin Chin <chinkaiwen@yahoo.com>
2022-09-21 02:17:55 +0800

    Added more code to the template.

--- ComputerDiscussForumBot/src/Main.cpp
+++ ComputerDiscussForumBot/src/Main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <dpp/dpp.h>
 
-void onBotReady(dpp::cluster &client) {
-
+void onBotReady(dpp::cluster &client, const dpp::ready_t &ev) {
+    if (dpp::run_once<struct register_bot_commands>()) {
+        client.global_command_create(dpp::slashcommand { "ping", "Ping Pong!", client.me.id });
+    }
 }
 
 int main(int argc, char** argv) {
     dpp::cluster client { "" };
-    client.on_ready([&client](const dpp::ready_t &event) { onBotReady(client); });
+    client.on_log(dpp::utility::cout_logger());
+    client.on_ready([&client](const dpp::ready_t &ev) { onBotReady(client, ev); });
     return 0;
 }
