cece000fca4a9a668cdeb8eae69e75d16bf3de8a
Kelvin Chin <chinkaiwen@yahoo.com>
2023-08-28 04:07:12 +0800

    Made ping command work

---
+++ src/commands/ping.rs
@@ -0,0 +1,7 @@
+use super::super::types::{Context, Error};
+
+#[poise::command(slash_command)]
+pub async fn ping(ctx: Context<'_>) -> Result<(), Error> {
+    ctx.say("Pong!").await?;
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,47 @@
 #[macro_use]
 extern crate lazy_static;
 
+mod commands;
 mod configs;
 mod services;
+mod types;
 
+use commands::*;
+use poise::serenity_prelude as serenity;
+use services::config_service::ConfigService;
 use std::{
     fs::File,
     io::{read_to_string, BufReader},
 };
+use tokio;
+use types::Data;
 
-use services::config_service::ConfigService;
-
-struct Test;
-
-impl Test {
-    pub fn run(&self) {
-        let cs = config_service!();
-        println!("This is from test class: {}", cs.get_document().get_token());
-    }
-}
-
-fn main() {
+#[tokio::main]
+async fn main() {
     let config_file_handler = File::open("config.toml").unwrap();
     let config_file_reader = BufReader::new(config_file_handler);
     let config_file = read_to_string(config_file_reader).unwrap();
     ConfigService::initialize(config_file);
+    let token: String;
 
     {
-        let config_service = config_service!();
-
-        print!("token: {}\n", config_service.get_document().get_token());
-        print!("app_id: {}\n", config_service.get_document().get_app_id());
-        print!(
-            "guild_id: {}\n",
-            config_service.get_document().get_guild_id()
-        );
-        println!(
-            "database: {}",
-            config_service
-                .get_document()
-                .get_database()
-                .get_pocketbase_domain()
-        );
+        let cs = config_service!();
+        token = cs.get_document().get_token().clone();
     }
 
-    let tester = Test {};
-    tester.run();
+    let framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            commands: vec![ping::ping()],
+            ..Default::default()
+        })
+        .token(token)
+        .intents(serenity::GatewayIntents::non_privileged())
+        .setup(move |ctx, _ready, framework| {
+            Box::pin(async move {
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(Data {})
+            })
+        });
+
+    framework.run().await.unwrap();
 }
