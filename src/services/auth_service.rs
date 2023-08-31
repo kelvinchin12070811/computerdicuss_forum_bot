ec2a81c8c9e430a8384aa939c14d189205a42e75
Kelvin Chin <chinkaiwen@yahoo.com>
2023-09-01 03:03:40 +0800

    Added refresh token function and move some error handling to guard clauses

--- src/services/auth_service.rs
+++ src/services/auth_service.rs
@@ -1,8 +1,10 @@
 use log::{debug, error};
+use tokio::sync::broadcast::error;
 
 use crate::{config_service, dtos::auth::Auth};
 use chrono::prelude::*;
-use std::{collections::HashMap, sync::Mutex};
+use chrono::Duration;
+use std::{collections::HashMap, ops::Add, sync::Mutex};
 #[macro_use]
 use super::config_service::ConfigService;
 
@@ -69,33 +71,72 @@ pub async fn login_db() {
         .send()
         .await;
 
-    match res {
-        Ok(data) => {
-            let data = data.json::<Auth>().await;
-            match data {
-                Ok(data) => {
-                    let mut auth_service = auth_service!();
-                    auth_service.set_token(data.get_token());
-                    auth_service.set_fetch_time(Utc::now());
-                }
-                Err(error) => error!("{}", error),
-            }
-        }
-        Err(error) => error!("{}", error),
+    if let Err(err) = res {
+        error!("{}", err);
+        return;
+    };
+
+    let data = res.unwrap().json::<Auth>().await;
+
+    if let Err(data) = data {
+        error!("{}", data);
+        return;
     }
+
+    let data = data.unwrap();
+    let mut auth_service = auth_service!();
+    auth_service.set_token(data.get_token());
+    auth_service.set_fetch_time(Utc::now());
+}
+
+pub async fn refresh_token() {
+    let configs = config_service!().get_document();
+    let mut auth_service = auth_service!();
+    let domain = configs.get_database().get_pocketbase_domain();
+    let token = auth_service.get_token();
+
+    let client = reqwest::Client::new();
+    let res = client
+        .post(format!("{}/api/collections/users/auth-refresh", domain))
+        .header("Authorization", format!("Bearer {}", token))
+        .send()
+        .await;
+    if let Err(error) = res {
+        error!("{}", error);
+        return;
+    }
+
+    let data = res.unwrap().json::<Auth>().await;
+    if let Err(error) = data {
+        error!("{}", error);
+        return;
+    }
+
+    let data = data.unwrap();
+    auth_service.set_token(data.get_token());
+    auth_service.set_fetch_time(Utc::now());
 }
 
 pub async fn get_token() -> String {
-    let token = {
+    let (token, fetched_time) = {
         let mut auth_service = auth_service!();
-        auth_service.get_token().to_owned()
+        (
+            auth_service.get_token().to_owned(),
+            auth_service.get_fetch_time().to_owned(),
+        )
     };
 
     if token.is_empty() {
         login_db().await;
         let mut auth_service = auth_service!();
-        auth_service.get_token().to_owned()
-    } else {
-        token.to_owned()
+        return auth_service.get_token().to_owned();
     }
+
+    if fetched_time.add(Duration::minutes(30)).timestamp() < Utc::now().timestamp() {
+        refresh_token().await;
+        let mut auth_service = auth_service!();
+        return auth_service.get_token().to_owned();
+    }
+
+    token.to_owned()
 }
